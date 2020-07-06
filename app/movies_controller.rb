0b18cbc95a55ca6c004da6b465d16fd1b26cc1f1
Luca <lucakanyo@protonmail.com>
2020-07-05 20:37:38 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,9 +28,10 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # title == "Home Alone"
   # release_date == 1990
   
-  Movie.create do |m|
-    m.title, m.release_date = args[:title], args[:release_date]
-  end
+  Movie.create(args)
+  #Movie.create do |m|
+    #m.title, m.release_date = args[:title], args[:release_date]
+  #end
 end
 
 def can_get_the_first_item_in_the_database
@@ -46,7 +47,8 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by_id(1)
+  Movie.find(1)
+  #Movie.find_by_id(1)
 end
 
 def can_find_by_multiple_attributes
@@ -54,7 +56,8 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by({title: "Title", release_date: 2000, director: "Me"})
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
+  #Movie.find_by({title: "Title", release_date: 2000, director: "Me"})
 end
 
 def can_find_using_where_clause_and_be_sorted
